b66549f14d49707d214de41886987e4bd5bfc2fd
ushiboy <ushiboy.dev@gmail.com>
2017-11-05 23:19:50 +0900

    add build task

--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,90 @@
 const host = process.env.ALLOW_ALL ? '0.0.0.0' : '127.0.0.1';
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const webpack = require('webpack');
+const path = require('path');
+const fs = require('fs');
 
-module.exports = {
-  entry: [
-    './src/app.js',
-    './src/app.css'
-  ],
-  output: {
-    filename: 'bundle.js',
-    path: __dirname
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
+function configureWebpack(environment) {
+  environment = environment || 'development';
+  const base = {
+    entry: [
+      './src/app.js',
+      './src/app.css'
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader'
+        },
+        {
+          test: /\.woff2?$|\.ttf$|\.eot$|\.svg$/,
+          loader: 'file-loader'
+        },
+        {
+          test: /\.(css|scss)$/,
+          loader: ExtractTextPlugin.extract('css-loader!postcss-loader!sass-loader')
+        }
+      ]
+    },
+    plugins: [
+      new ExtractTextPlugin({
+        filename:'bundle.css',
+        disable: false,
+        allChunks: true
+      }),
+      new webpack.LoaderOptionsPlugin({
+        options: {
+          postcss: [
+            require('postcss-easy-import')({
+              glob: true
+            }),
+            require('postcss-nested')
+          ]
+        }
+      })
+    ],
+  };
+
+  if (environment === 'production') {
+    fs.mkdirSync(path.join(__dirname, 'build'));
+    fs.copyFileSync(path.join(__dirname, 'index.html'), path.join(__dirname, 'build', 'index.html'));
+    return Object.assign({}, base, {
+      output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'build')
       },
-      {
-        test: /\.woff2?$|\.ttf$|\.eot$|\.svg$/,
-        loader: 'file-loader'
+      plugins: base.plugins.concat([
+        new webpack.DefinePlugin({
+          'process.env': {
+            NODE_ENV: JSON.stringify('production')
+          }
+        }),
+        new webpack.optimize.UglifyJsPlugin()
+      ])
+    });
+  } else {
+    return Object.assign({}, base, {
+      output: {
+        filename: 'bundle.js',
+        path: __dirname
       },
-      {
-        test: /\.(css|scss)$/,
-        loader: ExtractTextPlugin.extract('css-loader!postcss-loader!sass-loader')
-      }
-    ]
-  },
-  devServer: {
-    contentBase: '.',
-    inline: true,
-    host,
-    port: 8080,
-    disableHostCheck: true,
-    stats: {
-      version: false,
-      hash: false,
-      chunkModules: false
-    }
-  },
-  plugins: [
-    new ExtractTextPlugin({
-      filename:'bundle.css',
-      disable: false,
-      allChunks: true
-    }),
-    new webpack.LoaderOptionsPlugin({
-      options: {
-        postcss: [
-          require('postcss-easy-import')({
-            glob: true
-          }),
-          require('postcss-nested')
-        ]
-      }
-    })
-  ],
-  devtool: 'source-map'
-};
+      devServer: {
+        contentBase: '.',
+        inline: true,
+        host,
+        port: 8080,
+        disableHostCheck: true,
+        stats: {
+          version: false,
+          hash: false,
+          chunkModules: false
+        }
+      },
+      devtool: 'source-map'
+    });
+  }
+}
+
+module.exports = configureWebpack(process.env.ENV);
